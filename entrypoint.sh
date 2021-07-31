804dfe8e8f65b099cbc38753d00dfc4fdc22cf26
Jono Hill <jono@hillnz.com>
2021-08-01 10:12:40 +1200

    Background the up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,17 @@
 
 set -ef
 
+{
+    echo "Waiting for a moment before running 'up'..."
+    sleep 5
+    if [ "$AUTH_KEY" != "" ]; then
+        # shellcheck disable=SC2086
+        tailscale up --auth-key "$AUTH_KEY" $UP_ARGS
+    elif [ "$UP_ARGS" != "" ]; then
+        # shellcheck disable=SC2086
+        tailscale up $UP_ARGS
+    fi
+} &
+
 # shellcheck disable=SC2086
-tailscaled $DAEMON_ARGS & 
-
-if [ "$AUTH_KEY" != "" ]; then
-    # shellcheck disable=SC2086
-    tailscale up --auth-key "$AUTH_KEY" $UP_ARGS
-elif [ "$UP_ARGS" != "" ]; then
-    # shellcheck disable=SC2086
-    tailscale up $UP_ARGS
-fi
-
-fg
+exec tailscaled $DAEMON_ARGS
